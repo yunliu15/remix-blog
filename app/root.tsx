1c39ab8d1b10f5e52f7ef8b164a326bf66316b92
yun <caseyunliu@gmail.com>
2023-08-22 20:45:53 -0400

    initial code -- blog with admin user

---
+++ app/root.tsx
@@ -0,0 +1,52 @@
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction, LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
+
+import { getUser } from "~/session.server";
+import stylesheet from "~/tailwind.css";
+import { getEnv } from "./env.server";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export const loader = async ({ request }: LoaderArgs) => {
+  return json({ 
+    user: await getUser(request),
+    ENV: getEnv(),
+  });
+};
+
+export default function App() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <script 
+        dangerouslySetInnerHTML = {{__html: `window.ENV = ${JSON.stringify(data.ENV)}`}}
+        />      
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
